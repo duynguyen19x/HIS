566c36d862ce9677a7317abb6147e08f2ec627b4
duynguyen19x <105050939+duynguyen19x@users.noreply.github.com>
2023-11-19 19:18:59 +0700

    Update net7

--- HIS.ApplicationService/Business/InOutStockType/IInOutStockTypeService.cs
+++ HIS.ApplicationService/Business/InOutStockType/IInOutStockTypeService.cs
@@ -1,9 +1,4 @@
 ﻿using HIS.Dtos.Business.InOutStockTypes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HIS.ApplicationService.Business.InOutStockType
 {
