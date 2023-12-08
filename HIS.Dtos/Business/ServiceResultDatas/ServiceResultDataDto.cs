eb38dfb738a65ab02b6706934c6b7d7d654f3641
duynguyen19x <105050939+duynguyen19x@users.noreply.github.com>
2023-12-08 20:58:39 +0700

    .

---
+++ HIS.Dtos/Business/ServiceResultDatas/ServiceResultDataDto.cs
@@ -0,0 +1,16 @@
+﻿using HIS.Application.Core.Services.Dto;
+using HIS.Utilities.Enums;
+
+namespace HIS.Dtos.Business.ServiceResultDatas
+{
+    public class ServiceResultDataDto : EntityDto<Guid>
+    {
+        public Guid? ServiceResultIndiceId { get; set; }
+
+        public Guid? ServiceId { get; set; }
+
+        public string Result { get; set; }
+
+        public ServiceResultDataType ResultType { get; set; }
+    }
+}
