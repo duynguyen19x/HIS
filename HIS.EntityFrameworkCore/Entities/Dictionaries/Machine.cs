3d09507a03fc46fad6c1f756a9f31e6f4f2f56d1
HoangND <ng.duc.hoang9x1@gmail.com>
2024-01-30 02:21:08 +0700

    hoangnd commit machine

---
+++ HIS.EntityFrameworkCore/Entities/Dictionaries/Machine.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper.Configuration.Annotations;
+using HIS.Core.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.EntityFrameworkCore.Entities.Dictionaries
+{
+    public class Machine : AuditedEntity<Guid>
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int MachineType { get; set; } //Loại máy
+        public int MachineOrderType { get; set; } //Lọi máy đặt
+        public Guid? RoomId { get; set; } //Phòng
+        public Guid? DepartmentId { get; set; } //Khoa
+        public DateTime CreationDate { get; set; } //Ngày tạo
+        public string Creator { get; set; } //Người tạo
+        public string Description { get; set; } //Mô tả
+        public string UsageStandard { get; set; } //Định mức sử dụng
+        public bool Inactive { get; set; }
+
+        [Ignore]
+        public Room Room { get; set; }
+        [Ignore]
+        public Department Department { get; set; }
+    }
+}
