e3f9c55ee1a3fe0bdff58739da992c8f497483ba
hai-nx <72272795+hai-nx@users.noreply.github.com>
2024-02-08 01:57:00 +0700

    update

---
+++ HIS.EntityFrameworkCore/Entities/Systems/SYSAutoData.cs
@@ -0,0 +1,15 @@
+﻿using HIS.Core.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.EntityFrameworkCore.Entities.Systems
+{
+    [Table("SYSAutoData")]
+    public class SYSAutoData : AuditedEntity<Guid>
+    {
+    }
+}
