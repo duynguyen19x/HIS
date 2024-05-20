e890a5b7fd32d9e0f4fd961a8936fadfde8a1ea4
hai-nx <72272795+hai-nx@users.noreply.github.com>
2024-05-21 00:34:04 +0700

    update

--- HIS.EntityFrameworkCore/Entities/Dictionary/PaymentMethod.cs
+++ HIS.EntityFrameworkCore/Entities/Dictionary/PaymentMethod.cs
@@ -17,18 +17,18 @@ namespace HIS.EntityFrameworkCore.Entities
     {
         [Required]
         [MaxLength(50)]
-        public virtual string Code { get; set; }
+        public string Code { get; set; }
 
         [Required]
         [MaxLength(255)]
-        public virtual string Name { get; set; }
+        public string Name { get; set; }
 
         [MaxLength(255)]
-        public virtual string Description { get; set; }
+        public string Description { get; set; }
 
-        public virtual int SortOrder { get; set; }
+        public int SortOrder { get; set; }
 
-        public virtual bool Inactive { get; set; }
+        public bool Inactive { get; set; }
 
     }
 }
