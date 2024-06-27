8cf8aa0c20cdb566848badba01e62b0a92ea7d5d
hai-nx <72272795+hai-nx@users.noreply.github.com>
2024-06-27 22:01:59 +0700

    update

---
+++ HIS.EntityFrameworkCore/Entities/IcdChapter.cs
@@ -0,0 +1,14 @@
+﻿using HIS.Core.Domain.Entities;
+using HIS.Core.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.EntityFrameworkCore.Entities.Dictionary
+{
+    public class IcdChapter : AuditedEntity<Guid>
+    {
+    }
+}
