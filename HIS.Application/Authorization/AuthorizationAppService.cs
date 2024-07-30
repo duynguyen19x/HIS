dd95144997dc9d2a772d28e50dfea8cb4d70b2b4
hai-nx <72272795+hai-nx@users.noreply.github.com>
2024-07-30 22:55:30 +0700

    update

---
+++ HIS.Application/Authorization/AuthorizationAppService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Application.Authorization
+{
+    public class AuthorizationAppService : IAuthorizationAppService
+    {
+    }
+}
---
+++ HIS.Application/Business/Patients/PatientAppService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Application.Business.Patients
+{
+    public class PatientAppService : IPatientAppService
+    {
+        public PatientAppService() 
+        { 
+        }
+    }
+}
