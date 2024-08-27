e0bf769734c5d749ed3762bf23f2cb6b8b27c88d
duynguyen19x <105050939+duynguyen19x@users.noreply.github.com>
2024-08-27 21:45:48 +0700

    commit

--- HIS.ApplicationService/Dictionary/Services/Dto/ServiceDto.cs
+++ HIS.ApplicationService/Dictionary/Services/Dto/ServiceDto.cs
@@ -7,6 +7,10 @@ namespace HIS.ApplicationService.Dictionary.Services.Dto
 {
     public class ServiceDto : EntityDto<Guid?>
     {
+        IList<ServicePricePolicyDto> _sServicePricePolicies;
+        IList<ExecutionRoomDto> _sExecutionRooms;
+        IList<ServiceResultIndiceDto> _sServiceResultIndices;
+
         public string Code { get; set; }
 
         public string Name { get; set; }
@@ -16,7 +20,6 @@ namespace HIS.ApplicationService.Dictionary.Services.Dto
         public string HeInName { get; set; }
 
         public int? SortOrder { get; set; }
-
 
         public bool Inactive { get; set; }
 
@@ -34,8 +37,35 @@ namespace HIS.ApplicationService.Dictionary.Services.Dto
         public string ServiceGroupHeInName { get; set; }
         public string SurgicalProcedureTypeCode { get; set; }
 
-        public IList<ServicePricePolicyDto> SServicePricePolicies { get; set; }
-        public IList<ExecutionRoomDto> SExecutionRooms { get; set; }
-        public IList<ServiceResultIndiceDto> SServiceResultIndices { get; set; }
+        public IList<ServicePricePolicyDto> SServicePricePolicies
+        {
+            get
+            {
+                if (_sServicePricePolicies == null)
+                    _sServicePricePolicies = new List<ServicePricePolicyDto>();
+                return _sServicePricePolicies;
+            }
+            set => _sServicePricePolicies = value;
+        }
+        public IList<ExecutionRoomDto> SExecutionRooms
+        {
+            get
+            {
+                if (_sExecutionRooms == null)
+                    _sExecutionRooms = new List<ExecutionRoomDto>();
+                return _sExecutionRooms;
+            }
+            set => _sExecutionRooms = value;
+        }
+        public IList<ServiceResultIndiceDto> SServiceResultIndices
+        {
+            get
+            {
+                if (_sServiceResultIndices == null)
+                    _sServiceResultIndices = new List<ServiceResultIndiceDto>();
+                return _sServiceResultIndices;
+            }
+            set => _sServiceResultIndices = value;
+        }
     }
 }
