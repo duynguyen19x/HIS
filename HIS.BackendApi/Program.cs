45be0b58a54dfa949530056833c93e5b7eb307e4
duynguyen19x <105050939+duynguyen19x@users.noreply.github.com>
2024-10-31 20:44:42 +0700

    Commit

--- HIS.BackendApi/Program.cs
+++ HIS.BackendApi/Program.cs
@@ -131,11 +131,11 @@ void ConfigureService()
 void Configure()
 {
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    //if (app.Environment.IsDevelopment())
+    //{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //}
 
     app.UseCors("CorsPolicy");
     app.UseAuthentication();
